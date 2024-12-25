89b139016c9ac3f9b0944b654933a9c0939109b5
Martin Leshi <martinleshi@hotmail.com>
2024-12-25 13:24:29 +0100

    Init

---
+++ SourceGenerator/GeneratorHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace SourceGenerator;
+
+internal static class GeneratorHelper
+{
+    public static string GetNamespaceName(this SyntaxNode syntaxNode)
+        => syntaxNode switch
+        {
+            FileScopedNamespaceDeclarationSyntax fileScopedNamespace => fileScopedNamespace.Name.ToString(),
+            NamespaceDeclarationSyntax namespaceDeclaration => namespaceDeclaration.Name.ToString(),
+            _ => throw new ArgumentOutOfRangeException()
+        };
+}
